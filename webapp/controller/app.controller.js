53bf0a57cb943b57f1919b0019b59dd4e1d8a8f6
Sumin Lee <sm.lee9807@gmail.com>
2022-12-01 07:02:53 +0000

    기본 Routing 변경

--- webapp/controller/app.controller.js
+++ webapp/controller/app.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("fiexercise.controller.View1", {
+        return Controller.extend("fiexercise.controller.app", {
             onInit: function () {
 
             }
